789f1b6dc7e0085c287b55b85762befacc3164fd
Jong in Park <whddls9632@naver.com>
2019-07-23 14:43:09 +0900

    Make StyleSheet at App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.textBig, styles.textRed]}>Hello, World!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textBig: {
+    fontSize: 32,
+  },
+  textRed: {
+    color: 'red'
+  }
 });
