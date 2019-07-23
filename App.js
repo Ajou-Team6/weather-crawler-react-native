5a6723e2f4adced9424e2bc9436bfb876f1e482a
Jong in Park <whddls9632@naver.com>
2019-07-23 15:05:12 +0900

    Flex Components

--- App.js
+++ App.js
@@ -1,43 +1,33 @@
 import React from 'react';
 import { StyleSheet, Button, Text, View } from 'react-native';
 
-class CounterButton extends React.Component {
-  static defaultProps = {
-    counter: -1,
-  };
+// class CounterButton extends React.Component {
+//   static defaultProps = {
+//     counter: -1,
+//   };
 
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      counter: props.counter,
-    };
-  }
+//     this.state = {
+//       counter: props.counter,
+//     };
+//   }
 
-  clickHandler = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
-  
-  render() {
-    return (
-      <View style={styles.buttonContainer}>
-        <Text>{this.state.counter}</Text>
-        <Button title={'click me!'} onPress={this.clickHandler}/>
-      </View>
-    );
-  }
-}
+//   clickHandler = () => {
+//     this.setState({
+//       counter: this.state.counter + 1,
+//     });
+//   };
+// }
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CounterButton counter={0} />
-        <CounterButton count={1} />
-        <CounterButton counter={2} />
-        <CounterButton counter={3} />
+        <Text style={styles.text}>1</Text>
+        <Text style={[styles.text, styles.text2]}>2</Text>
+        <Text style={styles.text}>3</Text>
       </View>
     );
   }
@@ -47,10 +37,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    width: '100%',
+  text: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'red',
+
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  text2: {
+    flex: 2,
   }
 });
