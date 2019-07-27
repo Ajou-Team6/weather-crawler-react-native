4095546d0291d5a03a4515356cec8d33f9541f24
juhowoo <einjuho7@ajou.ac.kr>
2019-07-27 20:51:15 +0900

    Added DetailPage-weatherPicture&humidity&Description

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -4,6 +4,7 @@ import Constants from 'expo-constants';
 import config from './config/config';
 import {RotationHoleLoader} from 'react-native-indicator';
 
+var weatherCheck = require('./CheckWeatherPicture')
 
 export default class WeatherDetailScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -22,10 +23,10 @@ export default class WeatherDetailScreen extends React.Component {
 
   componentDidMount() {
     const { navigation } = this.props;
+    const city = navigation.getParam('city', null);
 
-    const city = 'Daejeon';
-
-    fetch(`http://demo6468405.mockable.io/weather-crawlers/current-weathers/by-city-name/${city}`)
+//    fetch('http://' + config.ip + ':' + config.port + '/weather-crawler/current-weathers/by-city-name/{city}')
+      fetch(`http://` + config.ip + `:` + config.port + `/weather-crawler/current-weathers/by-city-name/${city}`)
       .then(response => response.json())
       .then(info => {
         this.setState({
@@ -34,6 +35,7 @@ export default class WeatherDetailScreen extends React.Component {
         });
       });
   }
+
 
   render() {
     if (this.state.isLoading) {
@@ -44,17 +46,88 @@ export default class WeatherDetailScreen extends React.Component {
       )
     }
 
+
+// main-> 사진 and 글씨 표시
+// 온도
+// 습도
+// 바람
+
     let celsius = this.state.main.temp - 273.15;
     let weather = this.state.weather[0].main;
+    let humidity = this.state.main.humidity;
+    let icon = this.state.weather[0].icon;
+
+
+   switch (icon) {
+          case '01d':
+          imagePath = require('./assets/weather_icon/01d.png');
+          break;
+          case '01n':
+          imagePath = require('./assets/weather_icon/01n.png');
+          break;
+          case '02d':
+          imagePath = require('./assets/weather_icon/02d.png');
+          break;
+          case '02n':
+          imagePath = require('./assets/weather_icon/02n.png');
+          break;
+          case '03d':
+          imagePath = require('./assets/weather_icon/03d.png');
+          break;
+          case '03n':
+          imagePath = require('./assets/weather_icon/03n.png');
+          break;
+          case '04d':
+          imagePath = require('./assets/weather_icon/04d.png');
+          break;
+          case '04n':
+          imagePath = require('./assets/weather_icon/04n.png');
+          break;
+          case '09n':
+          imagePath = require('./assets/weather_icon/09n.png');
+          break;
+          case '09d':
+          imagePath = require('./assets/weather_icon/09d.png');
+          break;
+          case '10n':
+          imagePath = require('./assets/weather_icon/10n.png');
+          break;
+          case '10d':
+          imagePath = require('./assets/weather_icon/10d.png');
+          break;
+          case '11n':
+          imagePath = require('./assets/weather_icon/11n.png');
+          break;
+          case '11d':
+          imagePath = require('./assets/weather_icon/11d.png');
+          break;
+          case '13n':
+          imagePath = require('./assets/weather_icon/13n.png');
+          break;
+          case '13d':
+          imagePath = require('./assets/weather_icon/13d.png');
+          break;
+          case '50d':
+          imagePath = require('./assets/weather_icon/50d.png');
+          break;
+          case '50n':
+          imagePath = require('./assets/weather_icon/50n.png');
+          break;
+      }
+
 
     return (
       <View style={styles.container}>
 
        <Text style={styles.MainText}>{celsius.toFixed(1)}℃</Text>
+
+
         <Image
-            style={{width: 150, height: 150}}
-            source={require('./assets/Sunny.png')}
+                   style={{width: 150, height: 150}}
+                   source={imagePath}
         />
+
+        <Text style={styles.BaseText}>humidity: {humidity}</Text>
 
         <Text style={styles.BaseText}>{weather}</Text>
 
